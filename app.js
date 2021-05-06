9fc21b4767ed27f751f1f04b815059c1da497872
Elijah Ohiwerei <elijahohimairei@gmail.com>
2021-05-06 12:59:18 +0100

    files

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const playerS = document.querySelector('.player-score');
 const computerS = document.querySelector('.computer-score');
 const game = document.querySelector('#total-game');
 const remark = document.querySelector('.game-remark');
-let computerSelection = Math.floor(Math.random() * 9);
+const computerSelection = () => {
+    return Math.floor(Math.random() * 9);
+}
 
 
 
@@ -15,45 +17,45 @@ let finalScore = playerScore + computerScore;
 function playRound() {
 
     rock.addEventListener("click", () => {
-        if (computerSelection <= 3) {
+        if (computerSelection() <= 3) {
             computerScore++;
             computerS.innerText = computerScore;
             remark.innerText = 'Game Point Computer'
-        } else if (computerSelection > 3 && computerSelection <= 6) {
+        } else if (computerSelection() > 3 && computerSelection() <= 6) {
             playerScore++;
             playerS.innerText = playerScore;
             remark.innerText = 'Game Point Player'
-        } else if (computerSelection > 6 && computerSelection <= 10) {
+        } else if (computerSelection() > 6 && computerSelection() <= 10) {
             playerScore;
             computerScore;
             remark.innerText = 'Tie'
         } finalSel();
     })
     paper.addEventListener("click", () => {
-        if (computerSelection > 6 && computerSelection <= 10) {
+        if (computerSelection() > 6 && computerSelection() <= 10) {
             playerScore++;
             playerS.innerText = playerScore
             remark.innerText = 'Game Point Player'
-        } else if (computerSelection > 3 && computerSelection <= 6) {
+        } else if (computerSelection() > 3 && computerSelection() <= 6) {
             computerScore++;
             computerS.innerText = computerScore;
             remark.innerText = 'Game Point Computer'
-        } else if (computerSelection <= 3) {
+        } else if (computerSelection() <= 3) {
             playerScore;
             computerScore;
             remark.innerText = 'Tie'
         } finalSel();
     })
     scissors.addEventListener("click", () => {
-        if (computerSelection <= 3) {
+        if (computerSelection() <= 3) {
             playerScore++;
             playerS.innerText = playerScore
             remark.innerText = 'Game Point Player'
-        } else if (computerSelection > 6 && computerSelection <= 10) {
+        } else if (computerSelection() > 6 && computerSelection() <= 10) {
             computerScore++;
             computerS.innerText = computerScore;
             remark.innerText = 'Game Point Computer'
-        } else if (computerSelection > 3 && computerScore <= 6) {
+        } else if (computerSelection() > 3 && computerSelection() <= 6) {
             playerScore;
             computerScore;
             remark.innerText = 'Tie'
